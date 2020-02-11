cb7b28e05548389c95a7ef3d9b5de694900e1ba6
Purnendu Das <das888purnendu@gmail.com>
2020-02-11 16:51:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#a_list = list(map(int,input().split())) # Space separated numbers -> 1 2 3 4 5
+
+a_list = [1,2,3,4,5] # Given list
+n = len(a_list)  # N items of the list
+k = 3        # lenght of subsequence
+print("Main List :",a_list)
+print("All Possible Combinations are :")
+if k>1:
+    for i in range(n):
+        for j in range(i+1,n):
+            if(len(a_list[j:j+k-1]))<k-1:
+                break
+            temp=[a_list[i]]
+            temp.extend(a_list[j:j+k-1])
+            print(*temp,sep=",")
+else:
+    print(*a_list,sep="\n")
